738558c219cb664f19e7fc1b425a0ce9edb98849
joaomarcoscostat <joaomarcoscosta13@hotmail.com>
2019-05-21 21:35:27 -0300

    insercao do js

---
+++ Atividade_Pratica_4/JavaScript2.js
@@ -0,0 +1,25 @@
+/*alert ("hello word")
+document.getElementById('nome').value="Nome"
+var texto="texto"
+var numerointeiro=12
+var numeroFracionado=121.48
+var teste=false//true->verdadeira/false->falso
+alert(texto, numeroFracionado,numerointeiro)
+document.write(texto)
+console.log(texto)
+
+var nome= prompt('Digite o seu nome')
+var idade2= prompt('Digite a sua idade')
+console.log(idade)
+console.log(idade2)
+document.write('<h1> Olá'+nome+',tudo bem? A sua idade é: '+idade2+' </h1>')*/
+
+if(6<=23){
+document.write('Entrou dentro de verdadeiro')
+}
+
+else{
+    
+    document.write('Entrou dentro do falso')
+}
+    
