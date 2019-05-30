96e8f7611c5aee10cad51153f6ad396f210e8168
joaomarcoscostat <joaomarcoscosta13@hotmail.com>
2019-05-29 21:41:22 -0300

    Exercicio 4 com praticas das aulas

---
+++ Atividade_Pratica_4/Pratica_1/JavaScript.js
@@ -0,0 +1,23 @@
+alert ("hello word")
+document.getElementById('nome').value="Nome"
+var texto="texto"
+var numerointeiro=12
+var numeroFracionado=121.48
+var teste=false//true->verdadeira/false->falso
+
+
+
+
+alert(texto, numeroFracionado,numerointeiro)
+document.write(texto, nomeFracionado, numerointeiro)
+console.log(texto)
+
+var nome= prompt('Digite o seu nome')
+var idade2= prompt('Digite a sua idade')
+
+
+
+
+console.log(idade)
+console.log(idade2)
+documen.write('<h1> Olá'+nome+',tudo bem? A sua idade é: '+idade+' </h1>')
