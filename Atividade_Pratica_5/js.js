5f6842c2a81b8058b8927ccacfb9f958f5fab48a
joaomarcoscostat <joaomarcoscosta13@hotmail.com>
2019-05-29 21:33:42 -0300

    Atividade 5 Media

---
+++ Atividade_Pratica_5/js.js
@@ -0,0 +1,20 @@
+
+var num1= prompt("Digite 1° numero")
+var num2= prompt("Digite 2° numero")
+var num3= prompt("Digite 3° numero")
+var num4= prompt("Digite 4° numero")
+
+num1=parseFloat(num1)
+num2=parseFloat(num2)
+num3=parseFloat(num3)
+num4=parseFloat(num4)
+
+var media=(num1+num2+num3+num4)/4
+
+
+
+document.write('<h1>Nota 1: '+num1+'</h1>')
+document.write('<h1>Nota 2: '+num2+'</h1>')
+document.write('<h1>Nota 3: '+num3+'</h1>')
+document.write('<h1>Nota 4: '+num4+'</h1>')
+document.write('<h1>A média é: '+media+'</h1>')
